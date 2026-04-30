e62be421f9f7ab292aa687bd85248238b6f3fcc6
itsrohitkumar4444-dot <its.rohit.kumar.4444@gmail.com>
2026-04-30 13:43:41 +0530

    Limit issue fixed

---
+++ new.py
@@ -0,0 +1,21 @@
+import random 
+upper_limit = int(input("enter the upper limit:"))
+lower_limit = int(input("enter the lower limit:"))
+if upper_limit<= lower_limit:
+    number = random.randint(lower_limit, upper_limit)
+    print("you have only 5 chances to guess the number")    
+    for i in range(1,6):
+        guess = int(input("Enter your Guess :"))
+        if guess == number:
+            print("Congratulations you guessed the number")
+            break
+        else:
+            print("wrong guess")
+            print(f"you have {5-i} chances left")
+            if guess < number:
+                print("your guess is low")
+            else:
+                print("your guess is high")
+    print(f"You were unable to guess the number which is {number}")
+else : 
+    print("You have Entered larger lower limit than upper limit.")
